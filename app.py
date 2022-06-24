f0ba3c5d5cac71e3ce28cd14c60262d2098e9f19
takishun0326 <takishun.st3260@gmail.com>
2022-06-24 16:44:24 +0900

    solve syntax error

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-    PostbackEvent,FollowEvent
+    PostbackEvent,FollowEvent,
     QuickReply, QuickReplyButton
 )
 
